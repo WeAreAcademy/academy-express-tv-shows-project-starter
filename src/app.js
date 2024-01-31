4dbe44ae8cf120259fcf3f81c63c7741da3014d0
nbogie <neillbogie@googlemail.com>
2024-01-31 12:36:46 +0000

    initial from main academy express template

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const { app } = require("./support/setupExpress");
+const { query } = require("./support/db");
+
+//configure the server's route handlers
+app.get("/", (req, res) => {
+    res.render("pages/index");
+});
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const dbResult = await query("select now()");
+        const rows = dbResult.rows;
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(
+            "Sorry, an error occurred on the server.  Ask the dev team to check the server logs at time " +
+                new Date(),
+        );
+    }
+});
+
+// use the environment variable PORT, or 3000 as a fallback if it is undefined
+const PORT_NUMBER = process.env.PORT ?? 3000;
+
+//start the server listening indefinitely
+app.listen(PORT_NUMBER, () => {
+    console.log(
+        `Your express app started listening on ${PORT_NUMBER} at ${new Date()}`,
+    );
+});
